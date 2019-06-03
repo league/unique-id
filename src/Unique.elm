04cf9713d921becefb147961ce61e61565294734
Christopher League <league@contrapunctus.net>
2019-06-03 16:23:35 -0400

    Run elm-format

--- src/Unique.elm
+++ src/Unique.elm
@@ -1,101 +1,160 @@
 module Unique exposing
-  ( Id
-  , Unique
-  , andThen
-  , map
-  , map2
-  , map3
-  , replicate
-  , return
-  , run
-  , sequence
-  , unique
-  )
+    ( Id, Unique
+    , run
+    , return, unique
+    , andThen, map, map2, map3, replicate, sequence
+    )
 
-{-|
-Pure generation of unique identifiers in Elm.
+{-| Pure generation of unique identifiers in Elm.
+
 
 # Types
+
 @docs Id, Unique
 
+
 # Run
+
 @docs run
 
+
 # Introduction
+
 @docs return, unique
 
+
 # Sequencing
+
 @docs andThen, map, map2, map3, replicate, sequence
 
 -}
+
 
 {-| The type of an identifier. Identifiers are integers, but this is an opaque
 type so that we cannot introduce them by accident. Equality and toString are
 supported on `Id` values.
 -}
-type Id = Id Int
+type Id
+    = Id Int
+
 
 {-| A computation that consumes unique identifiers and produces a value of type
 `a`.
 -}
-type Unique a = U (Id -> (a, Id))
+type Unique a
+    = U (Id -> ( a, Id ))
 
-{-| Private: treat a Unique as a function. -}
-open : Unique a -> Id -> (a, Id)
-open (U f) = f
 
-{-| Private: generate next ID. -}
+{-| Private: treat a Unique as a function.
+-}
+open : Unique a -> Id -> ( a, Id )
+open (U f) =
+    f
+
+
+{-| Private: generate next ID.
+-}
 next : Id -> Id
-next (Id n) = Id (n+1)
+next (Id n) =
+    Id (n + 1)
 
-{-| Return a constant value without consuming any IDs. -}
+
+{-| Return a constant value without consuming any IDs.
+-}
 return : a -> Unique a
-return a = U(\k -> (a, k))
+return a =
+    U (\k -> ( a, k ))
 
-{-| Generate a unique ID. -}
+
+{-| Generate a unique ID.
+-}
 unique : Unique Id
-unique = U(\k -> (k, next k))
+unique =
+    U (\k -> ( k, next k ))
 
-{-| Chain together two `Unique` computations. -}
+
+{-| Chain together two `Unique` computations.
+-}
 andThen : (a -> Unique b) -> Unique a -> Unique b
 andThen fb (U fa) =
-  U(\k0 -> let (a, k1) = fa k0 in open (fb a) k1)
+    U
+        (\k0 ->
+            let
+                ( a, k1 ) =
+                    fa k0
+            in
+            open (fb a) k1
+        )
+
 
 {-| Run a computation that consumes unique IDs. Within one invocation of `run`,
 the generated `Id` values are guaranteed to be unique. However, multiple
-invocations of `run` will generate conflicting `Id`s. -}
+invocations of `run` will generate conflicting `Id`s.
+-}
 run : Unique a -> a
-run (U f) = Tuple.first (f (Id 0))
+run (U f) =
+    Tuple.first (f (Id 0))
 
-{-| Apply a function to the result of a `Unique` computation. -}
+
+{-| Apply a function to the result of a `Unique` computation.
+-}
 map : (a -> b) -> Unique a -> Unique b
 map f ua =
-  ua |> andThen (\a -> return (f a))
+    ua |> andThen (\a -> return (f a))
 
-{-| Apply a function to the result of two `Unique` computations. -}
+
+{-| Apply a function to the result of two `Unique` computations.
+-}
 map2 : (a -> b -> c) -> Unique a -> Unique b -> Unique c
 map2 f ua ub =
-  ua |> andThen (\a -> ub |> andThen (\b -> return (f a b)))
+    ua |> andThen (\a -> ub |> andThen (\b -> return (f a b)))
 
-{-| Apply a function to the result of three `Unique` computations. -}
+
+{-| Apply a function to the result of three `Unique` computations.
+-}
 map3 : (a -> b -> c -> d) -> Unique a -> Unique b -> Unique c -> Unique d
 map3 f ua ub uc =
-  ua |> andThen (\a ->
-    ub |> andThen (\b ->
-      uc |> andThen (\c ->
-        return (f a b c))))
+    ua
+        |> andThen
+            (\a ->
+                ub
+                    |> andThen
+                        (\b ->
+                            uc
+                                |> andThen
+                                    (\c ->
+                                        return (f a b c)
+                                    )
+                        )
+            )
 
-{-| Repeatedly invoke a `Unique` computation, generating a list of results. -}
+
+{-| Repeatedly invoke a `Unique` computation, generating a list of results.
+-}
 replicate : Int -> Unique a -> Unique (List a)
 replicate n f =
-  if n <= 0 then return []
-  else f |> andThen (\head ->
-    replicate (n-1) f |> andThen (\tail ->
-      return (head::tail)))
+    if n <= 0 then
+        return []
 
-{-| Execute a list of `Unique` computations in order. -}
-sequence : List(Unique a) -> Unique(List a)
+    else
+        f
+            |> andThen
+                (\head ->
+                    replicate (n - 1) f
+                        |> andThen
+                            (\tail ->
+                                return (head :: tail)
+                            )
+                )
+
+
+{-| Execute a list of `Unique` computations in order.
+-}
+sequence : List (Unique a) -> Unique (List a)
 sequence list =
-  case list of
-    [] -> return []
-    (head::tail) -> map2 (::) head (sequence tail)
+    case list of
+        [] ->
+            return []
+
+        head :: tail ->
+            map2 (::) head (sequence tail)
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,57 +1,65 @@
-module Tests exposing (..)
+module Tests exposing (tests)
 
 import Debug exposing (toString)
-import Unique exposing (..)
 import Expect exposing (equal, false, notEqual)
 import Test exposing (Test, describe, test)
+import Unique exposing (..)
+
 
 tests : Test
-tests = describe "Unique IDs"
-        [ test "Constant is preserved"
-          <| \() -> equal 91
-          <| run
-          <| return 91
-
-        , test "Two uniques in the same run are different"
-          <| \() -> (\(x,y) -> notEqual x y)
-          <| run
-          <| map2 Tuple.pair unique unique
-
-        , test "But uniques in different runs are the same"
-          <| \() -> equal (run unique) (run unique)
-
-        , test "map toString works on IDs"
-          <| \() -> false "string shouldn't be empty"
-          <| String.isEmpty
-          <| run <| map toString unique
-
-        , test "use andThen to join values"
-          <| \() -> equal "(Id 0,15,Id 1)"
-          <| toString
-          <| run
-          <| (unique |> andThen (\x -> unique |> andThen (\y -> return (x,15,y))))
-
-        , test "Use map3 to join values"
-          <| \() -> equal "[Id 0,Id 1,Id 2,Id 1]"
-          <| toString
-          <| run
-          <| map3 (\x y z -> [x,y,z,y]) unique unique unique
-
-        , test "Use replicate to create list of uniques"
-          <| \() -> equal "[Id 0,Id 1,Id 2,Id 3,Id 4]"
-          <| toString
-          <| run
-          <| replicate 5 unique
-
-        , test "Use replicate to create list of constants"
-          <| \() -> equal "[9,9,9,9,9]"
-          <| toString
-          <| run
-          <| replicate 5 <| return 9
-
-        , test "Use sequence to run through a list"
-          <| \() -> equal ["Ok","Id 0","Now","Id 1"]
-          <| run
-          <| sequence [return "Ok", map toString unique,
-                       return "Now", map toString unique]
+tests =
+    describe "Unique IDs"
+        [ test "Constant is preserved" <|
+            \() ->
+                equal 91 <|
+                    run <|
+                        return 91
+        , test "Two uniques in the same run are different" <|
+            \() ->
+                (\( x, y ) -> notEqual x y) <|
+                    run <|
+                        map2 Tuple.pair unique unique
+        , test "But uniques in different runs are the same" <|
+            \() -> equal (run unique) (run unique)
+        , test "map toString works on IDs" <|
+            \() ->
+                false "string shouldn't be empty" <|
+                    String.isEmpty <|
+                        run <|
+                            map toString unique
+        , test "use andThen to join values" <|
+            \() ->
+                equal "(Id 0,15,Id 1)" <|
+                    toString <|
+                        run <|
+                            (unique |> andThen (\x -> unique |> andThen (\y -> return ( x, 15, y ))))
+        , test "Use map3 to join values" <|
+            \() ->
+                equal "[Id 0,Id 1,Id 2,Id 1]" <|
+                    toString <|
+                        run <|
+                            map3 (\x y z -> [ x, y, z, y ]) unique unique unique
+        , test "Use replicate to create list of uniques" <|
+            \() ->
+                equal "[Id 0,Id 1,Id 2,Id 3,Id 4]" <|
+                    toString <|
+                        run <|
+                            replicate 5 unique
+        , test "Use replicate to create list of constants" <|
+            \() ->
+                equal "[9,9,9,9,9]" <|
+                    toString <|
+                        run <|
+                            replicate 5 <|
+                                return 9
+        , test "Use sequence to run through a list" <|
+            \() ->
+                equal [ "Ok", "Id 0", "Now", "Id 1" ] <|
+                    run <|
+                        sequence
+                            [ return "Ok"
+                            , map toString unique
+                            , return "Now"
+                            , map toString unique
+                            ]
         ]
